3553677c74f202d824aea1d2ec9fd8ba74b00fb5
PEDRO LUCAS ARAUJO LIMA <pedro.lalima@senacsp.edu.br>
2025-01-16 14:49:16 -0300

    first commit

---
+++ Lista_de_exercicios_01/Lista_01/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Objetivo: Pratique a comparação de variáveis. 
+Descrição: Escreva um programa que solicite ao usuário dois números e determine qual é o maior. Use if, else if e else para exibir a mensagem apropriada. 
+Exemplo de saída: "O primeiro número é maior.", "O segundo número é maior." ou "Os números são iguais.
+ */
+
+Console.WriteLine("Digite um número");
+int n1 = int.Parse (Console.ReadLine());
+
+
+
+if (n1 % 2 == 0)
+    Console.WriteLine("O número que voce digitou é par");
+
+else
+    Console.WriteLine("O número que voce digitou é ínpar");
