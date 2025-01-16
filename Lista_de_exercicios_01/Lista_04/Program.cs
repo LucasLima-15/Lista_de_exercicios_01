99138d59bf15b65c81f778b3808fb90c9732b17d
PEDRO LUCAS ARAUJO LIMA <pedro.lalima@senacsp.edu.br>
2025-01-16 15:25:43 -0300

    first commit

--- Lista_de_exercicios_01/Lista_04/Program.cs
+++ Lista_de_exercicios_01/Lista_04/Program.cs
@@ -6,3 +6,16 @@ Objetivo: Familiarizar -se com a verificação de intervalos numéricos.
 Descrição: Crie um programa que verifique se um número inserido pelo usuário está dentro do intervalo de 1 a 100. Use uma condição if para verificar se o número está dentro do intervalo e exiba uma mensagem informando o resultado. 
 Exemplo de saída: "O número está entre 1 e 100." ou "O número não está entre 1 e 100."
    */
+
+Console.WriteLine("Digite um número");
+float num  = float.Parse(Console.ReadLine());
+
+
+if (num > 100)
+    Console.WriteLine("O seu número não está entre 1 a 100");
+
+else if (num <= 0)
+    Console.WriteLine("O seu número não está entre 1 a 100");
+
+else if (num >= 1 )
+    Console.WriteLine("O seu numero está entre 1 a 100");
--- Lista_de_exercicios_01/Lista_04/obj/Debug/net8.0/Lista_04.AssemblyInfo.cs
+++ Lista_de_exercicios_01/Lista_04/obj/Debug/net8.0/Lista_04.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lista_04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a1dde8c441556a700d19cd5747667926eade80c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lista_04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lista_04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
